20300b178a5a8fc6001b7c4495181e9fb517e8d4
mino827 <61026964+mino827@users.noreply.github.com>
2022-01-04 18:28:59 +1300

    Changes automatically made by GRC
    Changes automatically made by

--- pocsag_demod.py
+++ pocsag_demod.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-##################################################
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
 # GNU Radio Python Flow Graph
 # Title: Pocsag Demod
-# Generated: Wed Mar 18 16:31:45 2020
-##################################################
+# GNU Radio version: 3.8.1.0
 
 from distutils.version import StrictVersion
 
@@ -16,27 +18,26 @@ if __name__ == '__main__':
             x11 = ctypes.cdll.LoadLibrary('libX11.so')
             x11.XInitThreads()
         except:
-            print "Warning: failed to XInitThreads()"
+            print("Warning: failed to XInitThreads()")
 
 from PyQt5 import Qt
-from PyQt5 import Qt, QtCore
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
 from gnuradio import analog
+import math
 from gnuradio import blocks
 from gnuradio import digital
-from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
-from gnuradio import qtgui
-from gnuradio.eng_option import eng_option
-from gnuradio.filter import firdes
-from optparse import OptionParser
-import math
-import osmosdr
-import sip
 import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import osmosdr
 import time
 from gnuradio import qtgui
-
 
 class pocsag_demod(gr.top_block, Qt.QWidget):
 
@@ -63,10 +64,13 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
 
         self.settings = Qt.QSettings("GNU Radio", "pocsag_demod")
 
-        if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-            self.restoreGeometry(self.settings.value("geometry").toByteArray())
-        else:
-            self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
 
         ##################################################
         # Variables
@@ -74,44 +78,49 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
         self.udp_port = udp_port = 5125
         self.symbol_rate_2 = symbol_rate_2 = 1200
         self.symbol_rate = symbol_rate = 1200
-        self.samp_rate = samp_rate = 1.8e6
+        self.samp_rate = samp_rate = 2e6
         self.ip_addr = ip_addr = "127.0.0.1"
-        self.fsk_deviation_hz = fsk_deviation_hz = 2.5e3
+        self.fsk_deviation_hz = fsk_deviation_hz = 4.5e3
         self.decimation = decimation = 10
-        self.center_freq_2 = center_freq_2 = 158.103e6
-        self.center_freq = center_freq = 152.180e6
+        self.center_freq_2 = center_freq_2 = 157.953e6
+        self.center_freq = center_freq = 157.953e6
 
         ##################################################
         # Blocks
         ##################################################
-        self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + '' )
+        self.rtlsdr_source_0 = osmosdr.source(
+            args="numchan=" + str(1) + " " + ''
+        )
         self.rtlsdr_source_0.set_clock_source('internal', 0)
+        self.rtlsdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.rtlsdr_source_0.set_sample_rate(samp_rate)
         self.rtlsdr_source_0.set_center_freq(center_freq, 0)
         self.rtlsdr_source_0.set_freq_corr(0, 0)
-        self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
-        self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
-        self.rtlsdr_source_0.set_gain_mode(False, 0)
         self.rtlsdr_source_0.set_gain(10, 0)
         self.rtlsdr_source_0.set_if_gain(20, 0)
         self.rtlsdr_source_0.set_bb_gain(20, 0)
         self.rtlsdr_source_0.set_antenna('', 0)
         self.rtlsdr_source_0.set_bandwidth(samp_rate/2, 0)
-
-        self.root_raised_cosine_filter_0 = filter.fir_filter_fff(1, firdes.root_raised_cosine(
-        	1, samp_rate/decimation, symbol_rate, 0.35, 45))
+        self.root_raised_cosine_filter_0 = filter.fir_filter_fff(
+            1,
+            firdes.root_raised_cosine(
+                1,
+                samp_rate/decimation,
+                symbol_rate,
+                0.35,
+                45))
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-        	1024, #size
-        	samp_rate, #samp_rate
-        	"", #name
-        	1 #number of inputs
+            1024, #size
+            samp_rate, #samp_rate
+            "", #name
+            1 #number of inputs
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
         self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
 
         self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.enable_tags(True)
         self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
         self.qtgui_time_sink_x_0.enable_autoscale(False)
         self.qtgui_time_sink_x_0.enable_grid(False)
@@ -119,23 +128,22 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_0.enable_control_panel(False)
         self.qtgui_time_sink_x_0.enable_stem_plot(False)
 
-        if not True:
-          self.qtgui_time_sink_x_0.disable_legend()
 
         labels = ['', '', '', '', '',
-                  '', '', '', '', '']
+            '', '', '', '', '']
         widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "blue"]
-        styles = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-                   -1, -1, -1, -1, -1]
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
 
-        for i in xrange(1):
+
+        for i in range(1):
             if len(labels[i]) == 0:
                 self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
@@ -147,14 +155,14 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
             self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_freq_sink_x_1 = qtgui.freq_sink_c(
-        	1024, #size
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
-        	samp_rate, #bw
-        	"", #name
-        	1 #number of inputs
+            1024, #size
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            1
         )
         self.qtgui_freq_sink_x_1.set_update_time(0.10)
         self.qtgui_freq_sink_x_1.set_y_axis(-140, 10)
@@ -166,21 +174,18 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_1.enable_axis_labels(True)
         self.qtgui_freq_sink_x_1.enable_control_panel(False)
 
-        if not True:
-          self.qtgui_freq_sink_x_1.disable_legend()
 
-        if "complex" == "float" or "complex" == "msg_float":
-          self.qtgui_freq_sink_x_1.set_plot_pos_half(not True)
 
         labels = ['', '', '', '', '',
-                  '', '', '', '', '']
+            '', '', '', '', '']
         widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
+            1, 1, 1, 1, 1]
         colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
             if len(labels[i]) == 0:
                 self.qtgui_freq_sink_x_1.set_line_label(i, "Data {0}".format(i))
             else:
@@ -190,11 +195,11 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
             self.qtgui_freq_sink_x_1.set_line_alpha(i, alphas[i])
 
         self._qtgui_freq_sink_x_1_win = sip.wrapinstance(self.qtgui_freq_sink_x_1.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_1_win)
+        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_1_win)
         self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
-        	1024, #size
-        	"", #name
-        	1 #number of inputs
+            1024, #size
+            "", #name
+            1 #number of inputs
         )
         self.qtgui_const_sink_x_0.set_update_time(0.10)
         self.qtgui_const_sink_x_0.set_y_axis(-2, 2)
@@ -204,22 +209,21 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
         self.qtgui_const_sink_x_0.enable_grid(False)
         self.qtgui_const_sink_x_0.enable_axis_labels(True)
 
-        if not True:
-          self.qtgui_const_sink_x_0.disable_legend()
 
         labels = ['', '', '', '', '',
-                  '', '', '', '', '']
+            '', '', '', '', '']
         widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
+            1, 1, 1, 1, 1]
         colors = ["blue", "red", "red", "red", "red",
-                  "red", "red", "red", "red", "red"]
+            "red", "red", "red", "red", "red"]
         styles = [0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0]
+            0, 0, 0, 0, 0]
         markers = [0, 0, 0, 0, 0,
-                   0, 0, 0, 0, 0]
+            0, 0, 0, 0, 0]
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
             if len(labels[i]) == 0:
                 self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
@@ -231,13 +235,15 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
             self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(decimation, (firdes.low_pass(5, samp_rate, fsk_deviation_hz,fsk_deviation_hz*2)), 0, samp_rate)
+        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_win)
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(decimation, firdes.low_pass(5, samp_rate, fsk_deviation_hz,fsk_deviation_hz*2), 0, samp_rate)
         self.digital_clock_recovery_mm_xx_0 = digital.clock_recovery_mm_ff(samp_rate/(decimation*symbol_rate)*(1+0.0), 0.01, 0, 0.1, 0.01)
         self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
         self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_char*1472, ip_addr, udp_port, 1472, True)
         self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_char*1, 1472)
         self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(samp_rate/(2*math.pi*fsk_deviation_hz/8.0))
+
+
 
         ##################################################
         # Connections
@@ -275,22 +281,22 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
 
     def set_symbol_rate(self, symbol_rate):
         self.symbol_rate = symbol_rate
-        self.root_raised_cosine_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate/self.decimation, self.symbol_rate, 0.35, 45))
         self.digital_clock_recovery_mm_xx_0.set_omega(self.samp_rate/(self.decimation*self.symbol_rate)*(1+0.0))
+        self.root_raised_cosine_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate/self.decimation, self.symbol_rate, 0.35, 45))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.analog_quadrature_demod_cf_0.set_gain(self.samp_rate/(2*math.pi*self.fsk_deviation_hz/8.0))
+        self.digital_clock_recovery_mm_xx_0.set_omega(self.samp_rate/(self.decimation*self.symbol_rate)*(1+0.0))
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(5, self.samp_rate, self.fsk_deviation_hz,self.fsk_deviation_hz*2))
+        self.qtgui_freq_sink_x_1.set_frequency_range(0, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.root_raised_cosine_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate/self.decimation, self.symbol_rate, 0.35, 45))
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
         self.rtlsdr_source_0.set_bandwidth(self.samp_rate/2, 0)
-        self.root_raised_cosine_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate/self.decimation, self.symbol_rate, 0.35, 45))
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.qtgui_freq_sink_x_1.set_frequency_range(0, self.samp_rate)
-        self.freq_xlating_fir_filter_xxx_0.set_taps((firdes.low_pass(5, self.samp_rate, self.fsk_deviation_hz,self.fsk_deviation_hz*2)))
-        self.digital_clock_recovery_mm_xx_0.set_omega(self.samp_rate/(self.decimation*self.symbol_rate)*(1+0.0))
-        self.analog_quadrature_demod_cf_0.set_gain(self.samp_rate/(2*math.pi*self.fsk_deviation_hz/8.0))
 
     def get_ip_addr(self):
         return self.ip_addr
@@ -303,16 +309,16 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
 
     def set_fsk_deviation_hz(self, fsk_deviation_hz):
         self.fsk_deviation_hz = fsk_deviation_hz
-        self.freq_xlating_fir_filter_xxx_0.set_taps((firdes.low_pass(5, self.samp_rate, self.fsk_deviation_hz,self.fsk_deviation_hz*2)))
         self.analog_quadrature_demod_cf_0.set_gain(self.samp_rate/(2*math.pi*self.fsk_deviation_hz/8.0))
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(5, self.samp_rate, self.fsk_deviation_hz,self.fsk_deviation_hz*2))
 
     def get_decimation(self):
         return self.decimation
 
     def set_decimation(self, decimation):
         self.decimation = decimation
-        self.root_raised_cosine_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate/self.decimation, self.symbol_rate, 0.35, 45))
         self.digital_clock_recovery_mm_xx_0.set_omega(self.samp_rate/(self.decimation*self.symbol_rate)*(1+0.0))
+        self.root_raised_cosine_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate/self.decimation, self.symbol_rate, 0.35, 45))
 
     def get_center_freq_2(self):
         return self.center_freq_2
@@ -328,6 +334,7 @@ class pocsag_demod(gr.top_block, Qt.QWidget):
         self.rtlsdr_source_0.set_center_freq(self.center_freq, 0)
 
 
+
 def main(top_block_cls=pocsag_demod, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -338,6 +345,16 @@ def main(top_block_cls=pocsag_demod, options=None):
     tb = top_block_cls()
     tb.start()
     tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
 
     def quitting():
         tb.stop()
